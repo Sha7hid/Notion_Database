a3dd71710abf3d12f6212e74faa58231a02d1153
sha7hid <shahidshafadshahad@gmail.com>
2022-09-15 20:24:58 +0530

    update

--- database/src/App.js
+++ database/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const [name,setName] = useState("");
+  const [phoneNumber,setPhoneNumber] = useState("");
+  const [extraInfo,setExtraInfo] = useState("");
+  function submitFormToNotion(){
+    console.log("we are In"+ name);
+    fetch("http://localhost:9000/submitFormToNotion", {
+      method:"post",
+      headers:{
+        headers: {'Content-Type':'application/x-www-form-urlencoded'}
+        
+      },
+      body: JSON.stringify({
+        name:name,
+        phoneNumber:phoneNumber,
+        extraInfo:extraInfo
+
+      })
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+  <div className='App'>
+<div style={{maxWidth:"500px",margin:"0 auto"}}>
+<h1>Interested in learning more? Put your info below</h1>
+<p>Name</p>
+<input type="text" id="name" onChange={(e) => setName(e.target.value)}/>
+<p>Phone Number</p>
+<input type="text" id="phoneNumber" onChange={(e) => setPhoneNumber(e.target.value)}/>
+<p>Anything else?</p>
+<textarea onChange={(e) => setExtraInfo(e.target.value)} rows={10} cols={25}/>
+<div>
+  <button onClick={submitFormToNotion}>
+submit to Notion
+  </button>
+</div>
+</div>
+  </div>
   );
 }
 
--- database/src/server.js
+++ database/src/server.js
@@ -1,1 +1,63 @@
-console.log("hello")
+const express = require('express');
+const {Client} = require('@notionhq/client');
+const cors = require('cors');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+const app = express();
+
+app.use(cors());
+
+const PORT = 9000;
+const HOST = "localhost";
+
+const client = new Client({auth:"secret_5rNlMs8xTfgEBjeofc2diulOtltqQEAERtdS2ulLBhj"})
+
+const databaseid = "acb54fb7be574aa38d5377635551354a";
+
+app.post('/submitFormToNotion',jsonParser, async(req,res)=>{
+const name = req.body.name;
+const phoneNumber= req.body.phoneNumber;
+const extraInfo = req.body.extraInfo;
+try{
+const response = await Notification.pages.create({
+    parent : {databaseid: databaseid},
+    properties:{
+        Name : {
+            title:[
+                {
+                    text:{
+                        content: name
+                    }
+                }
+            ]
+        },
+        "Phone Number" : {
+            rich_text:[
+                {
+                    text:{
+                        content: phoneNumber
+                    }
+                }
+            ]
+        },
+        "Extra Information" : {
+            rich_text:[
+                {
+                    text:{
+                        content: extraInfo
+                    }
+                }
+            ]
+        }
+    }
+})
+console.log(response);
+console.log("SUCCESS !")
+} catch(error){
+    console.log(error);
+}
+});
+app.listen(PORT, HOST,  ()=> {
+console.log("starting proxy at "+ HOST + "!" + PORT);
+})

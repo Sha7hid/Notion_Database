8daeff530e70a5423d322f12d28fdf11054aa7f4
sha7hid <shahidshafadshahad@gmail.com>
2022-09-18 11:36:58 +0530

    update

--- index.js
+++ index.js
@@ -4,36 +4,51 @@ const notion = new Client({ auth: process.env.NOTION_KEY })
 
 const databaseId = process.env.NOTION_DATABASE_ID
 
-async function addItem(text) {
-  try {
-    const response = await notion.pages.create({
-      parent: { database_id: databaseId },
-      properties: {
-        title: { 
-          title:[
-            {
-              "text": {
-                "content": text
-              }
-            }
-          ]
-        },
-        phoneno:{
-            number:[
-                {
-                    "number":{
-                        "content": number
+async function addToDatabase(databaseId, name, number, extra) {
+    try {
+        const response = await notion.pages.create({
+            parent: {
+                database_id: databaseId,
+            },
+            properties: {
+                'Name': {
+                    type: 'title',
+                    title: [
+                    {
+                        type: 'text',
+                        text: {
+                            content: name,
+                        },
+                    },
+                    ],
+                },
+                'PhoneNumber' : {
+                        type: 'rich_text',
+                        rich_text: [
+                        {
+                            type: 'text',
+                            text: {
+                                content: number,
+                            },
+                        }
+                        ],
+                },
+                'ExtraInformation' : {
+                    type: 'rich_text',
+                    rich_text: [
+                    {
+                        type: 'text',
+                        text: {
+                            content: extra,
+                        },
                     }
-                }
-            ]
-        }
-
-      },
-    })
-    console.log(response)
-    console.log("Success! Entry added.")
-  } catch (error) {
-    console.error(error.body)
-  }
+                    ],
+            },
+            }    
+        });
+        console.log(response);
+    } catch (error) {
+        console.error(error.body);
+    }
 }
-addItem("Shahid",123)
+addToDatabase(databaseId, 'shahid','7994959712','haise');
